f4d5e158423ac0e850713d54e814687b0f71e494
rafa moral <rafamp89@gmail.com>
2023-04-18 15:33:58 +0200

    feat: create docs structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {DocsThemeConfig} from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template'
-  },
-  chat: {
-    link: 'https://discord.com'
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template'
-  }
+    logo: <span>Calsott</span>,
+    project: {
+        link: 'https://github.com/calsott/ci-cli'
+    },
+    docsRepositoryBase: 'https://github.com/calsott/docs/tree/main/pages',
+    editLink: {
+        text: 'Edit this page on GitHub'
+    },
+    footer: {
+        text: 'Calsott docs'
+    }
 }
 
 export default config
